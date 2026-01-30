c0d72738187a968cb94422e308d85c8dc9b052fa
TTKhumalo <treasuretapelo@gmail.com>
2026-01-30 06:29:40 -0800

    updating files

---
+++ math.test.js
@@ -0,0 +1,10 @@
+// math.test.js
+const { add, multiply } = require('./math');
+
+test('adds 2 + 3 to equal 5', () => {
+  expect(add(2, 3)).toBe(5);
+});
+
+test('multiplies 2 * 3 to equal 6', () => {
+  expect(multiply(2, 3)).toBe(6);
+});
